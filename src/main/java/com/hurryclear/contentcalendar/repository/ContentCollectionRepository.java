4202f592af4cd66258a2b9d38ca6d185c7a36f72
hurryclear <hurryclear@mail.de>
2023-12-14 20:37:03 +0100

    Add methods: save and init to ContentCollectionRepository

--- src/main/java/com/hurryclear/contentcalendar/repository/ContentCollectionRepository.java
+++ src/main/java/com/hurryclear/contentcalendar/repository/ContentCollectionRepository.java
@@ -1,8 +1,12 @@
 package com.hurryclear.contentcalendar.repository;
 
 import com.hurryclear.contentcalendar.model.Content;
+import com.hurryclear.contentcalendar.model.Status;
+import com.hurryclear.contentcalendar.model.Type;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -15,18 +19,37 @@ This is not talking to database, but to keep a collection of pieces of content i
 
 public class ContentCollectionRepository {
 
-    private final List<Content> content = new ArrayList<>();
+    private final List<Content> contentList = new ArrayList<>();
 
     public ContentCollectionRepository() {
     }
 
+    // Get
     public List<Content> findAll() {
-        return content;
+        return contentList;
     }
 
     public Optional<Content> findById(Integer id) {
-        return content.stream().filter(c -> c.id().equals(id)).findFirst();
+        return contentList.stream().filter(c -> c.id().equals(id)).findFirst();
     }
 
 
+    public void save(Content content) {
+        contentList.add(content);
+    }
+
+    @PostConstruct
+    private void init() {
+        Content content = new Content(
+                1,
+                "My First Blog Post.",
+                "my first blog post",
+                Status.IDEA,
+                Type.ARTICLE,
+                LocalDateTime.now(),
+                null,
+                ""
+        );
+        contentList.add(content);
+    }
 }
