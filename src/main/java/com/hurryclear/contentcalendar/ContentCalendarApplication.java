97936e03dd8ce508443c39d8b44f0b345f590fff
hurryclear <hurryclear@mail.de>
2023-12-14 20:38:33 +0100

    Add main class: ContentCalendarApplication

--- src/main/java/com/hurryclear/contentcalendar/ContentCalendarApplication.java
+++ src/main/java/com/hurryclear/contentcalendar/ContentCalendarApplication.java
@@ -2,12 +2,18 @@ package com.hurryclear.contentcalendar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ContentCalendarApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ContentCalendarApplication.class, args);
+//		RestTemplate restTemplate = (RestTemplate) context.getBean("restTemplate");
+//		System.out.println(restTemplate);
 	}
 
+	// write more code
 }
