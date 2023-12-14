78b05135ad8d9afa52b0f3fe49759678b0590c43
hurryclear <hurryclear@mail.de>
2023-12-14 20:38:06 +0100

    Add Message class

---
+++ src/main/java/com/hurryclear/contentcalendar/Message.java
@@ -0,0 +1,11 @@
+package com.hurryclear.contentcalendar;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Message {
+
+    public String getMessage() {
+        return "Hello World!";
+    }
+}
