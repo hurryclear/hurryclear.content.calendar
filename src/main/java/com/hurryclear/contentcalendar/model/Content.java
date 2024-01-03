7705eda7999dfb198932c12812f4d99e10e7bbee
hurryclear <hurryclear@mail.de>
2024-01-03 19:07:30 +0100

    add annotation of identifier to id

--- src/main/java/com/hurryclear/contentcalendar/model/Content.java
+++ src/main/java/com/hurryclear/contentcalendar/model/Content.java
@@ -1,10 +1,15 @@
 package com.hurryclear.contentcalendar.model;
 
 import jakarta.validation.constraints.NotBlank;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
 
+// @Table(value = "tbl_content")
 public record Content(
+
+        @Id // an identifier
         Integer id,
         @NotBlank
         String title,
